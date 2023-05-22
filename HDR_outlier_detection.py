21e71b8a713e284984c00aa5f444e83f213fe982
Abhishek Sriram <abhisheksriram16@gmail.com>
2023-05-22 16:42:47 -0700

    Upd script for OD

--- HDR_outlier_detection.py
+++ HDR_outlier_detection.py
@@ -1,13 +1,29 @@
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 from pyspark.sql import SparkSession
+
+
+def show_dataframe(df):
+    df.show()
+
+
+def count_dataframe(df):
+    return df.count()
+
+
+def get_dataframe_length(df):
+    return len(df.columns)
+
+
+def print_dataframe_schema(df):
+    df.printSchema()
 
 
 def main():
     # Reading the csv file
-    df = pd.read_csv('Hospitalization_Discharge_Rates.csv')
-    n = len(df.columns)
+    spark = SparkSession.builder.appName("Outlier Detection").getOrCreate()
+    df = spark.read.csv("Hospitalization_Discharge_Rates.csv")
+
+    print_dataframe_schema(df)
+
     return 0
 
 
